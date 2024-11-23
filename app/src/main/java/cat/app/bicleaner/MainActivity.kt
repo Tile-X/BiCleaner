c91e16d7a4fa859f5059e602f4d4c7c153823365
Tile-X <ghh20030403@gmail.com>
2024-11-23 16:34:03 +0800

    remove themes

--- app/src/main/java/cat/app/bicleaner/MainActivity.kt
+++ app/src/main/java/cat/app/bicleaner/MainActivity.kt
@@ -8,24 +8,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import cat.app.bicleaner.ui.theme.BiCleanerTheme
-import de.robv.android.xposed.XposedHelpers
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BiCleanerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(
-                        text = if (MainActivity.isModuleActive()) "模块启用" else "模块未启用",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Text(
+                    text = if (isModuleActive()) "模块启用" else "模块未启用",
+                    modifier = Modifier.padding(innerPadding)
+                )
             }
         }
     }
